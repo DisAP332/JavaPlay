079da757391df75a5ed43a4fa538ad1340ec07a1
DAP <disap332@gmail.com>
2022-09-20 19:21:29 -0400

    Played with functions learned: Scope: where a function is being kept weather on a global stage ( whole program) or just within brackets learned functions are useful for calling out a script throughout the program to avoid code duplication You invoke a function by simply calling out the function name anonymous functions are functions without a name assigned to them return values simply returns the output of the function which can also be called out by a =>

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+/*
 let message = 'hello'
   , age = 25
   , user = 'John';
@@ -64,3 +65,134 @@ if (hour < 10) {
 
 console.log(greeting)
     ;
+
+//end//
+
+
+
+
+const myText = 'the weather is nice today'
+;
+const newString = myText.replace ('weather', 'temperature')
+;
+console.log(newString)
+;
+
+function favoriteAnimal(animal) {
+    console.log(animal + ' is my favorite animal!')
+}
+
+favoriteAnimal('ardvark')
+;
+
+let userName = 'John';
+
+function showMessage() {
+  let userName = "Bob"; // declare a local variable
+
+  let message = 'Hello, ' + userName; // Bob
+  alert(message);
+}
+
+// the function will create and use its own userName
+showMessage();
+
+alert( userName ); // John, unchanged, the function did not access the outer variable
+
+function showMessage(from, text) { // parameters: from, text
+    alert(from + ': ' + text);
+  }
+  
+  showMessage('Ann', 'Hello!'); // Ann: Hello! (*)
+  showMessage('Ann', "What's up?"); // Ann: What's up? (**)
+
+ //end
+
+
+
+
+
+
+
+function ask(question, yes, no) {
+    if (confirm(question)) 
+    yes()
+    else no()
+    ;
+}
+
+function showOk() {
+    alert ('you agreed.')
+    ;
+}
+
+function showCancel() {
+    alert ('You canceled the execution.')
+    ;
+}
+
+ask("Do you agree?", showOk, showCancel)
+
+function ask(question, yes, no) {
+    if (confirm(question)) 
+    yes()
+    else no()
+    ;
+}
+
+ask(
+    "do you agree?"
+    ,
+    function() {alert('you agreed'); }
+    ,
+    function () {alert('you canceled the execution.');}
+); 
+*/
+    /* let seven = 7
+    ,   eight = 8
+    ,   sum = seven + eight
+    ;
+    console.log (sum)
+    ; */
+
+function add7() {
+    a = 7
+    b = 2
+    sum1 = a + b
+    ;
+    console.log (sum1)
+}
+;
+function multiply() {
+    a = 3
+    b = 4
+    sum2 = a * b
+    ;
+    console.log (sum2)
+}
+;
+function addTogether() {
+    sum3 = sum1 + sum2 
+    ;
+    console.log (sum3)
+}
+
+function capitalize() {
+    string = 'capitalize'
+    newString = string.charAt(0).toUpperCase() + string.slice(1)
+    console.log (newString)
+    }
+
+
+function lastLetter() {
+    string = 'abcd'
+    newString = string.slice(3)
+    console.log (newString)
+
+}
+
+lastLetter()
+capitalize()
+add7()
+multiply()
+addTogether()
